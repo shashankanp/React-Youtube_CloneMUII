53385acf38153c258506e5c4608800adc5ef995f
shashankanp <shashanknp02@gmail.com>
2023-08-24 23:53:27 +0530

    Nearly done

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,9 +4,15 @@ import { Sidebar, Videos } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
-  useEffect(()={
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-  },[])
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -16,7 +22,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -33,10 +42,10 @@ const Feed = () => {
           mb={2}
           sx={{ color: "white" }}
         >
-          New <span style={{ color: "#F31503" }}>videos</span>
+          {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
 
-        <Videos videos={[]}/>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Box, Stack } from "@mui/material";
+import React from "react";
+import { VideoCard, ChannelCard } from "./";
 
-const Videos = () => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return "Loading...";
   return (
-    <div>Videos</div>
-  )
-}
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.kind == "youtube#video" && <VideoCard video={item} />}
+          {item.id.kind == "youtube#channel" && (
+            <ChannelCard channelDetail={item} />
+          )}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
 
-export default Videos
+export default Videos;
